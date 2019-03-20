54021d9d0f1a37b66d2fa1e33d811f805a1b2513
袁云峰 <yuanyunfeng@shinesaas.com>
2019-03-20 15:46:04 +0800

    完成

--- api/api/goods.js
+++ api/api/goods.js
@@ -1,8 +1,57 @@
 const router = require('koa-router')()
+const mongoose = require('mongoose')
 
 router.post('/getGoodDetail',async(ctx)=>{
-    console.log(ctx.request.body)
-    ctx.body = ctx.request.body
-    // ctx.body = 123
+    try {
+        let goodid = ctx.request.body.goodsId
+        const Goods = mongoose.model('Goods')
+        await Goods.findOne({ID: goodid}).exec().then(async(res)=>{
+            ctx.body = { code: 200, message: res}
+        })
+    }catch(err){
+        ctx.body = {
+            code: 500,
+            message: err
+        }
+    }
+})
+router.get('/getCatagoryList',async(ctx)=>{
+    try {
+        const Catagory = mongoose.model('catagory')
+        let res = await Catagory.find().exec()
+        ctx.body = {
+            code: 200,
+            message: res
+        }
+    }catch(err){
+        ctx.body = {
+            code: 500,
+            message: err
+        }
+    }
+})
+router.post('/getCategorySubList',async(ctx)=>{
+    try{
+        let categoryId = ctx.request.body.categoryId
+        const CategorySub = mongoose.model('catagorySub')
+        let result = await CategorySub.find({MALL_CATEGORY_ID:categoryId}).exec()
+        ctx.body={code:200,message:result}
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
+})
+router.post('/getGoodsListByCategorySubID',async(ctx)=>{
+    try{
+        let categorySubId = ctx.request.body.categoryId
+        let page = ctx.request.body.page
+        let num = 10
+        let start = (page-1)*num
+        const Goods = mongoose.model('Goods')
+        let result = await Goods.find({SUB_ID:categorySubId})
+        .skip(start) .limit(num).exec()
+        ctx.body={code:200,message:result}
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
 })
 module.exports = router
